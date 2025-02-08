d303200061a7d33688a0dc62319ff6ec9eb7278a
benjasx <benjasanchez175@gmail.com>
2025-02-08 15:36:45 -0700

    API-REST-FULL

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <p className="text-red-500 text-3xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
